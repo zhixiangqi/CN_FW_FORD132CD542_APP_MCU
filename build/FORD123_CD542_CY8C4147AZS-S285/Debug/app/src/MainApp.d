8003dba83709575b7abad88e809e650b7d31186e
AUO ChengyuChen <jeffreychen@alum.ccu.edu.tw>
2024-07-31 10:00:11 +0800

    [Revise] Chancel Ignore for project for build.

---
+++ build/FORD123_CD542_CY8C4147AZS-S285/Debug/app/src/MainApp.d
@@ -0,0 +1,253 @@
+D:/SourceCode/ModusWorkspace/CN_FW_FORD132CD542_APP_MCU/build/FORD123_CD542_CY8C4147AZS-S285/Debug/app/src/MainApp.o: \
+ app/src/MainApp.c main.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_pdl.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_device.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/cy_device_headers.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/cy8c4147azs_s285.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/core_cm0plus.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_version.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_compiler.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_gcc.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/mpu_armv7.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/system_cat2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/psoc4100sp_config.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_sflash_256.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_headers.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_peri.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_hsiom.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_srsslt.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_gpio.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_prgio.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_cpuss_v3.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_dmac_v3.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_spcif_v3.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_tcpwm_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_wco.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_scb_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_csd_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_lcd_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_lpcomp_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_crypto_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_can.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_exco.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_ctbm.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_sar.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_pass.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/gpio_psoc4100sp_64_tqfp.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_canfd.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syslib.h \
+ ../mtb_shared/core-lib/release-v1.4.2/include/cy_utils.h \
+ ../mtb_shared/core-lib/release-v1.4.2/include/cy_result.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_aes.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_common.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_hw.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_crc.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_prng.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_trng.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_sha.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_common.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_aes.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_vu.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_sha.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_trng.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_rsa.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_csd.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_ctb.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_dmac.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_flash.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_gpio.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_i2s.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_lpcomp.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syspm.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_msc.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_msclp.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sar.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_ezi2c.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_common.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_i2c.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_spi.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_uart.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_seglcd.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_smartio.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sysclk.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_wdt.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sysint.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_systick.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_counter.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_pwm.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_quaddec.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_trigmux.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbfs_dev_drv.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbfs_dev_drv_reg.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_bch.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_common.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_defines.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_regs.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_hpd.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_common.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_idac_ctrl.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_mux.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_phy.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_typec.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_vbus_ctrl.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_wdc.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/cybsp.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/cybsp_types.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/cybsp_doc.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_notices.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_system.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sysclk.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syspm.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_gpio.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_clocks.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_routing.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_peripherals.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sar.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_spi.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_i2c.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_uart.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_wdt.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_counter.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_pwm.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_pins.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/cybsp_hw_config.h \
+ app/inc/MainApp.h app/inc/TC0App.h app/inc/StackTaskApp.h \
+ app/inc/RegisterApp.h app/inc/BacklightApp.h app/inc/I2C2SlaveApp.h \
+ app/inc/INTBApp.h app/inc/DeviceApp.h app/inc/WdtApp.h \
+ app/inc/PowerApp.h app/inc/FlashApp.h driver/inc/UartDriver.h \
+ driver/inc/AdcDriver.h driver/inc/I2C4MDriver.h driver/inc/PwmDriver.h
+main.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_pdl.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_device.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/cy_device_headers.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/cy8c4147azs_s285.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/core_cm0plus.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_version.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_compiler.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_gcc.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/mpu_armv7.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/system_cat2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/psoc4100sp_config.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_sflash_256.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_headers.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_peri.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_hsiom.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_srsslt.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_gpio.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_prgio.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_cpuss_v3.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_dmac_v3.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_spcif_v3.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_tcpwm_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_wco.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_scb_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_csd_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_lcd_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_lpcomp_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_crypto_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_can.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_exco.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_ctbm.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_sar.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_pass.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/gpio_psoc4100sp_64_tqfp.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_canfd.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syslib.h:
+../mtb_shared/core-lib/release-v1.4.2/include/cy_utils.h:
+../mtb_shared/core-lib/release-v1.4.2/include/cy_result.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_aes.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_common.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_hw.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_crc.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_prng.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_trng.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_crypto_sha.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_common.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_aes.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_vu.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_sha.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_trng.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_cryptolite_rsa.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_csd.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_ctb.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_dmac.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_flash.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_gpio.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_i2s.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_lpcomp.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syspm.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_msc.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_msclp.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sar.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_ezi2c.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_common.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_i2c.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_spi.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_uart.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_seglcd.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_smartio.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sysclk.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_wdt.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sysint.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_systick.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_counter.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_pwm.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_quaddec.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_trigmux.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbfs_dev_drv.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbfs_dev_drv_reg.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_bch.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_common.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_defines.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_regs.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_hpd.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_common.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_idac_ctrl.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_mux.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_phy.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_typec.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_usbpd_vbus_ctrl.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_wdc.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/cybsp.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/cybsp_types.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/cybsp_doc.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_notices.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_system.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sysclk.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syspm.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_gpio.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_clocks.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_routing.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_peripherals.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sar.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_spi.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_i2c.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_scb_uart.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_wdt.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_counter.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_tcpwm_pwm.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/config/GeneratedSource/cycfg_pins.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/cybsp_hw_config.h:
+app/inc/MainApp.h:
+app/inc/TC0App.h:
+app/inc/StackTaskApp.h:
+app/inc/RegisterApp.h:
+app/inc/BacklightApp.h:
+app/inc/I2C2SlaveApp.h:
+app/inc/INTBApp.h:
+app/inc/DeviceApp.h:
+app/inc/WdtApp.h:
+app/inc/PowerApp.h:
+app/inc/FlashApp.h:
+driver/inc/UartDriver.h:
+driver/inc/AdcDriver.h:
+driver/inc/I2C4MDriver.h:
+driver/inc/PwmDriver.h:
---
+++ build/FORD123_CD542_CY8C4147AZS-S285/Debug/ext/mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/source/cy_flash.d
@@ -0,0 +1,86 @@
+D:/SourceCode/ModusWorkspace/CN_FW_FORD132CD542_APP_MCU/build/FORD123_CD542_CY8C4147AZS-S285/Debug/ext/mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/source/cy_flash.o: \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/source/cy_flash.c \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_flash.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_device.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/cy_device_headers.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/cy8c4147azs_s285.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/core_cm0plus.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_version.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_compiler.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_gcc.h \
+ ../mtb_shared/cmsis/release-v5.8.0/Core/Include/mpu_armv7.h \
+ bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/system_cat2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/psoc4100sp_config.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_sflash_256.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_headers.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_peri.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_hsiom.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_srsslt.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_gpio.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_prgio.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_cpuss_v3.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_dmac_v3.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_spcif_v3.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_tcpwm_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_wco.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_scb_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_csd_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_lcd_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_lpcomp_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_crypto_v2.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_can.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_exco.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_ctbm.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_sar.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_pass.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/gpio_psoc4100sp_64_tqfp.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syslib.h \
+ ../mtb_shared/core-lib/release-v1.4.2/include/cy_utils.h \
+ ../mtb_shared/core-lib/release-v1.4.2/include/cy_result.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sysclk.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syspm.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_wdt.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_device.h \
+ ../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syslib.h
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_flash.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_device.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/cy_device_headers.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/cy8c4147azs_s285.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/core_cm0plus.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_version.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_compiler.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/cmsis_gcc.h:
+../mtb_shared/cmsis/release-v5.8.0/Core/Include/mpu_armv7.h:
+bsps/TARGET_FORD123_CD542_CY8C4147AZS-S285/system_cat2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/psoc4100sp_config.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_sflash_256.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_headers.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_peri.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_hsiom.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_srsslt.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_gpio.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_prgio.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_cpuss_v3.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_dmac_v3.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_spcif_v3.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_tcpwm_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_wco.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_scb_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_csd_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_lcd_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_lpcomp_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_crypto_v2.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_can.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_exco.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_ctbm.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_sar.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/ip/cyip_pass.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/devices/include/gpio_psoc4100sp_64_tqfp.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syslib.h:
+../mtb_shared/core-lib/release-v1.4.2/include/cy_utils.h:
+../mtb_shared/core-lib/release-v1.4.2/include/cy_result.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_sysclk.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syspm.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_wdt.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_device.h:
+../mtb_shared/mtb-pdl-cat2/release-v2.11.0/drivers/include/cy_syslib.h:
